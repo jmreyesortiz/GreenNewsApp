69734089737effe685e0fb9ee69f2f085fbe36d5
juanmareyesortiz <juanmareyesortiz@gmail.com>
2020-01-22 22:25:18 -0600

    Final Commit

--- app/src/main/java/com/juanma/greennews/util/Constants.java
+++ app/src/main/java/com/juanma/greennews/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 
     public static final String BASE_URL = "https://newsapi.org";
-    public static final String API_KEY = "318797f5265a4ab69552508790391f6c";
+    public static final String API_KEY = "Your Own API Key";
     public static final int NETWORK_TIMEOUT = 3000;
     public static final String PAGE_SIZE = "20";
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
